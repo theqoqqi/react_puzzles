368c9ba44d1fc8d7ba67723063cfea1e66b8825b
Qoqqi <sazykin.georgii@gmail.com>
2022-09-03 16:00:48 +0300

    Добавлен скрипт для создания новых пазлов

--- src/pages/PuzzleList/PuzzleList.js
+++ src/pages/PuzzleList/PuzzleList.js
@@ -3,16 +3,7 @@ import React from 'react';
 import PuzzleLink from '../../components/PuzzleLink/PuzzleLink.js';
 import {Link} from 'react-router-dom';
 import LocalStorage from '../../core/LocalStorage.js';
-import Puzzle1 from '../puzzles/Puzzle1/Puzzle1.js';
-import Puzzle2 from '../puzzles/Puzzle2/Puzzle2.js';
-import Puzzle3 from '../puzzles/Puzzle3/Puzzle3.js';
-import Puzzle4 from '../puzzles/Puzzle4/Puzzle4.js';
-import Puzzle5 from '../puzzles/Puzzle5/Puzzle5.js';
-import Puzzle6 from '../puzzles/Puzzle6/Puzzle6.js';
-import Puzzle7 from '../puzzles/Puzzle7/Puzzle7.js';
-import Puzzle8 from '../puzzles/Puzzle8/Puzzle8.js';
-import Puzzle9 from '../puzzles/Puzzle9/Puzzle9.js';
-import Puzzle10 from '../puzzles/Puzzle10/Puzzle10.js';
+import * as allPuzzles from '../puzzles/allPuzzles.js';
 
 export default class PuzzleList extends React.Component {
 
@@ -20,18 +11,12 @@ export default class PuzzleList extends React.Component {
         // TODO: propTypes
     };
 
-    static allPuzzles = [
-        Puzzle1,
-        Puzzle2,
-        Puzzle3,
-        Puzzle4,
-        Puzzle5,
-        Puzzle6,
-        Puzzle7,
-        Puzzle8,
-        Puzzle9,
-        Puzzle10,
-    ];
+    static allPuzzles = Object.values(allPuzzles).sort((a, b) => {
+        let aNumber = +a.name.slice(6);
+        let bNumber = +b.name.slice(6);
+
+        return aNumber - bNumber;
+    });
 
     render() {
         let puzzles = PuzzleList.allPuzzles;
---
+++ src/pages/puzzles/allPuzzles.js
@@ -0,0 +1,11 @@
+
+export { default as Puzzle1 } from '../puzzles/Puzzle1/Puzzle1.js';
+export { default as Puzzle2 } from '../puzzles/Puzzle2/Puzzle2.js';
+export { default as Puzzle3 } from '../puzzles/Puzzle3/Puzzle3.js';
+export { default as Puzzle4 } from '../puzzles/Puzzle4/Puzzle4.js';
+export { default as Puzzle5 } from '../puzzles/Puzzle5/Puzzle5.js';
+export { default as Puzzle6 } from '../puzzles/Puzzle6/Puzzle6.js';
+export { default as Puzzle7 } from '../puzzles/Puzzle7/Puzzle7.js';
+export { default as Puzzle8 } from '../puzzles/Puzzle8/Puzzle8.js';
+export { default as Puzzle9 } from '../puzzles/Puzzle9/Puzzle9.js';
+export { default as Puzzle10 } from '../puzzles/Puzzle10/Puzzle10.js';
